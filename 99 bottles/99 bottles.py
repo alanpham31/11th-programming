0660e03e64af7ed54036c2a095a077830e3dd9ff
alanpham31 <70214028+alanpham31@users.noreply.github.com>
2021-04-26 08:00:16 -0600

    Add files via upload

---
+++ 99 bottles/99 bottles.py
@@ -0,0 +1,7 @@
+for i in range (99,0,-1):
+    if i > 1:
+        print(i, "Bottles of coke on the wall,", i, " bottles of coke,")
+        print("Take one down and pass it around,", i, "bottles of coke on the wall.\n")
+    else:
+        print(i, "Bottle of coke on the wall,", i, " bottle of coke,")
+        print("Take one down and pass it around,", i, "bottles of beer on the wall.\n")
