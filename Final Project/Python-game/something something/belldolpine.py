9215898b5756ab484b2194e50428bcb158ad6ff9
alanpham31 <70214028+alanpham31@users.noreply.github.com>
2021-05-30 10:21:04 -0600

    Add files via upload

---
+++ Final Project/Python-game/something something/belldolpine.py
@@ -0,0 +1,22 @@
+#Sophia Turner
+import random
+import time
+class Belldolpine:
+  #member variables
+  hp = 10
+  exp = 0
+  #basicDamage = int(random(7,9))
+  #specialDamage = int(random(11,13))
+  #hpRegain = int(random(3,5))
+  #constructor
+
+  #methods
+  #def basicAttack(self):
+    #damage = self.basicDamage
+    #print('You did ', self.basicDamage, ' damage!')
+  #def specialAttack2(self):
+    #damage = self.specialDamage
+    #print('You did ', self.specialDamage, ' Electric damage!')
+  #def heal(self):
+    #regain = self.hpRegain
+    #print('You regained ', self.hpRegain, ' health!')
